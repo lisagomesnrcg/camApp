f6fc7e6669b6e2a8cba25aa445a1d08dac6ea8e4
lisagomesnrcg <naiaracarolinegomes@gmail.com>
2023-06-06 21:51:58 -0300

    CAMAPP

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FotoService } from '../services/foto.service';
+import { Foto } from '../models/Foto.model';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,35 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public fotoService: FotoService, public actionsheetController: ActionSheetController) { }
+  
+  async ngOnInit () {
+    await this.fotoService.carregarFotosSalvas();
+  }
 
+  public async showActionSheet (foto: Foto, position: number) {
+    const actionsheet = await this.actionsheetController.create ({
+      header: 'Fotos',
+      buttons: [{
+        text: 'Delete',
+        role: 'Destructive',
+        icon: 'Trash',
+        handler: () => {
+          this.fotoService.deletePicture(foto, position);
+        }
+      }, {
+        text: 'Calcelar',
+        icon: 'Close',
+        role: 'Cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+        }
+      }]
+    });
+    await actionsheet.present();
+  }
+
+  tirarFoto() {
+    this.fotoService.tirarFoto();
+  }
 }
